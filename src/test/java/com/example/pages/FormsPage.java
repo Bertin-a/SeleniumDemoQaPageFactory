002357e9f5721df98096f4e9de89bb21d0448790
Bertina Ayuure <bertina.ayuure@amalitech.com>
2023-06-21 10:34:51 +0000

    calender

---
+++ src/test/java/com/example/pages/FormsPage.java
@@ -0,0 +1,15 @@
+package com.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+
+// page_url = about:blank
+public class FormsPage {
+
+    public FormsPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+}
